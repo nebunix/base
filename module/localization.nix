915a3905b33fbc6ec335202008a091a613982f12
Silvan Schmidt <mail@silvanschmidt.ch>
2025-02-16 14:05:12 +0100

    feat: configure xkb layout

--- module/localization.nix
+++ module/localization.nix
@@ -22,11 +22,35 @@ in
       default = "en_US.UTF-8";
       description = "The locale for your system.";
     };
+
+    xkb = {
+      layout = lib.mkOption {
+        type = with lib.types; str;
+        default = "us";
+        description = "The keyboard layout for applications that use xkb.";
+      };
+
+      variant = lib.mkOption {
+        type = with lib.types; str;
+        default = "";
+        description = "The keyboard variant for applications that use xkb.";
+      };
+    };
   };
 
   config = {
     console.keyMap = cfg.consoleKeyMap;
     time.timeZone = cfg.timeZone;
     i18n.defaultLocale = cfg.locale;
+
+    services.xserver.xkb = {
+      layout = cfg.xkb.layout;
+      variant = cfg.xkb.variant;
+    };
+
+    environment.sessionVariables = {
+      XKB_DEFAULT_LAYOUT = cfg.xkb.layout;
+      XKB_DEFAULT_VARIANT = cfg.xkb.variant;
+    };
   };
 }
