5e75aa8a785a169c8bbf01a1d18b3a3ec4ec60b4
Silvan Schmidt <mail@silvanschmidt.ch>
2025-02-16 16:31:12 +0100

    feat: set default shell

--- module/default-programs.nix
+++ module/default-programs.nix
@@ -6,15 +6,21 @@ in
 {
   options.nebunix.defaultPrograms = {
     terminal = lib.mkOption {
-      type = with lib.types; str;
-      default = "kitty";
+      type = with lib.types; nullOr str;
+      default = null;
       description = "The default terminal to use.";
     };
 
     browser = lib.mkOption {
-      type = with lib.types; str;
-      default = "firefox";
+      type = with lib.types; nullOr str;
+      default = null;
       description = "The default browser to use.";
+    };
+
+    shell = lib.mkOption {
+      type = with lib.types; str;
+      default = "bash";
+      description = "The default shell to use.";
     };
   };
 }
